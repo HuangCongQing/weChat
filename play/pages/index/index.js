43c1263a69ce1b27964f6bef10595422fa727612
HuangChongqing <1756260160@qq.com>
2017-08-05 09:20:12 +0800

    demo新版本

--- play/pages/index/index.js
+++ play/pages/index/index.js
@@ -9,7 +9,7 @@ Page({
   //事件处理函数
   bindViewTap: function() {
     console.log(this);
-    console.log(this.data.motto);
+    // console.log(this.data.motto);
     //页面跳转
     wx.navigateTo({
       url: '../logs/logs'
@@ -21,6 +21,7 @@ Page({
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
+      // console.log("userInfo"+userInfo)
       //更新数据
       that.setData({
         userInfo:userInfo
