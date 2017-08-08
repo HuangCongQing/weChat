f261896b25555b082ba5284bc2a9df36b35d34fc
HuangCongQing <1756260160Qqq.com>
2017-08-08 23:21:16 +0800

    练习

---
+++ Practice/app.js
@@ -0,0 +1,17 @@
+App({
+  onLaunch: function () {
+    //小程序加载时使用
+  },
+  onShow: function () {
+   // 小程序展示时使用
+  },
+  onHide: function () {
+   // 小程序隐藏时使用g3
+  },
+  onError: function (msg) {
+    //小程序报错时使用
+  },
+  //还可以添加一些属性
+  //全局属性，任意位置访问
+  hello: "你好"
+})
---
+++ Practice/pages/index/index.js
@@ -0,0 +1,45 @@
+/*
+    当前页面脚本文件
+ */
+
+Page({
+  data: {
+    text: "This is page data."
+  },
+  onLoad: function(options) {
+    // Do some initialize when page load.
+  },
+  onReady: function() {
+    // Do something when page ready.
+  },
+  onShow: function() {
+    // Do something when page show.
+  },
+  onHide: function() {
+    // Do something when page hide.
+  },
+  onUnload: function() {
+    // Do something when page close.
+  },
+  onPullDownRefresh: function() {
+    // Do something when pull down.
+  },
+  onReachBottom: function() {
+    // Do something when page reach bottom.
+  },
+  onShareAppMessage: function () {
+   // return custom share data when user share.
+  },
+  onPageScroll: function() {
+    // Do something when page scroll
+  },
+  // Event handler.
+  viewTap: function() {
+    this.setData({
+      text: 'Set some data for updating view.'
+    })
+  },
+  customData: {
+    hi: 'MINA'
+  }
+})
